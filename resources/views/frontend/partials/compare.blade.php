620bd4c4a0bbdbc619edc666805784736f35911d
Md Rasel Khan <immdraselkhan@gmail.com>
2023-02-17 03:03:32 +0600

    first commit

---
+++ resources/views/frontend/partials/compare.blade.php
@@ -0,0 +1,11 @@
+<a href="{{ route('compare') }}" class="d-flex align-items-center text-black">
+    <i class="la la-refresh la-2x"></i>
+    <span class="flex-grow-1 ml-1">
+        @if(Session::has('compare'))
+            <span class="badge badge-primary badge-inline badge-pill">{{ count(Session::get('compare'))}}</span>
+        @else
+            <span class="badge badge-primary badge-inline badge-pill">0</span>
+        @endif
+        <span class="nav-box-text d-none d-xl-block">{{translate('Compare')}}</span>
+    </span>
+</a>
